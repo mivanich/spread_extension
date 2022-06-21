2f591da0cf954e532792a1e67d3333446a339ff5
michel <redtrain.main@gmail.com>
2022-06-22 02:09:41 +0300

    Added spread for list of coins

--- app.js
+++ app.js
@@ -36,13 +36,13 @@ function createSpreadContainer(spread) {
 }
 
 
-if (document.URL.startsWith("https://zoloto-md.ru/bullion-coins/")) {
+if (false && document.URL.startsWith("https://zoloto-md.ru/bullion-coins/")) { // TODO: uncomment
     var labelSpread = document.createElement("label");
     
     var page = document.getElementById('page');
     
     var price_product = page.getElementsByClassName("product_price")[0]
-    var priceStr = price_product.childNodes[5].content;
+    var priceStr = price_product.childNodes[5].content; // THAT CAUSES ISSUE ON THE PAGES, DIFFERENT FROM PAGES WITH A SINGLE COIN
     var sellPrice = parseInt(priceStr);
     
     var buyoutDiv = page.getElementsByClassName("js-price-buyout")[0];
@@ -59,3 +59,66 @@ if (document.URL.startsWith("https://zoloto-md.ru/bullion-coins/")) {
     }
 }
 
+
+if (document.URL.startsWith("https://zoloto-md.ru/")) {
+    /*var cssRule = document.styleSheets[2].cssRules[3455];
+    if (!cssRule) {
+        console.log("cssRule 3455 is not found");
+    } else {
+        cssRule.style.maxHeight = '240px'; // change constants!
+    }*/
+    
+    var sheet = document.createElement('style')
+    sheet.innerHTML = ".product-list_item:hover .hover-price { max-height: 240px; }";
+    document.body.appendChild(sheet);
+    
+    var sheet2 = document.createElement('style')
+    sheet2.innerHTML = ".product_price2>span{display:inline-block;width:67%;text-align:left;font-size:24px;font-weight:700;padding-left:10px;color:#ff9112}";
+    document.body.appendChild(sheet2);
+   
+    
+    var coins = document.getElementsByClassName("pi-col-lg-4 pi-col-md-4 pi-col-xs-6");
+    if (coins) {
+        for (var i = 0; i < coins.length; i++) {
+            var coin = coins[i];
+            var sell = coin.getElementsByClassName("js-price")[0]
+            var buyout = coin.getElementsByClassName("js-price-buyout")[0]
+            
+            var sellPriceStr = sell ? sell.textContent : null;
+            var buyoutPriceStr = buyout ? buyout.textContent : null;
+            
+            if (sellPriceStr && buyoutPriceStr) {
+                var sellPrice = parseInt(sellPriceStr.replace(" ", ""));
+                var buyoutPrice = parseInt(buyoutPriceStr.replace(" ", ""));
+                // console.log("sell: " + sellPrice + ", buy: " + buyoutPrice);
+                var spread = (sellPrice - buyoutPrice) / buyoutPrice * 100;
+                console.log("Spread: " + spread.toFixed(2));
+                
+                var hoverPrice = coin.getElementsByClassName("hover-price")[0];
+                var spreadGeneralContainer = document.createElement("div");
+                spreadGeneralContainer.setAttribute("class", "product_price2");
+                hoverPrice.appendChild(spreadGeneralContainer);
+                
+                var spreadDescription = document.createElement("label");
+                spreadDescription.textContent = "Спред";
+                spreadGeneralContainer.appendChild(spreadDescription);
+                
+                var spreadValueGeneralContainer = document.createElement("span");
+                spreadGeneralContainer.appendChild(spreadValueGeneralContainer);
+                
+                var spreadActualValue = document.createElement("span");
+                spreadActualValue.setAttribute("class", "js-price-club2");
+                spreadActualValue.textContent = spread.toFixed(2) + " %";
+                spreadValueGeneralContainer.appendChild(spreadActualValue);
+                
+                // hoverPrice.firstElementChild.remove(); // TODO: maybe make something with it? 
+                
+                
+            } else {
+                console.log("Price not set: " + sellPriceStr + " | " + buyoutPriceStr);
+            }
+            
+            //console.log("Coin " + i + ". Sell: " + sell.textContent + ", buyout " + (buyout ? buyout.textContent : "undefined"));
+        }
+    }
+}
